e32f5bbddc0a455161c16e7944bc78652d7903cf
DuelistRag3 <christian.dullin@web.de>
2025-08-12 12:28:32 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,5 @@ Route::get('/', function () {
 Route::get('/{address}/{port}', function ($address, $port) {
     $address = gethostbyname($address);
     $newAddress = "steam://connect/{$address}:{$port}";
-    // return $newAddress;
     return redirect()->to($newAddress);
 });
