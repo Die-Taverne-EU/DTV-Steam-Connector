4636c7c0abdbb2c3d296d4023a048ae502230653
DuelistRag3 <christian.dullin@web.de>
2025-08-12 11:44:47 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 404;
+});
+
+Route::get('/{address}/{port}', function ($address, $port) {
+    $address = gethostbyname($address);
+    $newAddress = "steam://connect/{$address}:{$port}";
+    // return $newAddress;
+    return redirect()->to($newAddress);
 });
